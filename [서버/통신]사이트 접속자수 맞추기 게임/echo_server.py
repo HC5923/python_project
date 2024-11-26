fc4b37e2d966a3ef70786c973a1ca09bc306b928
HC5923 <5625923@naver.com>
2024-11-26 14:41:53 +0900

    =

--- [서버/통신]사이트 접속자수 맞추기 게임/echo_server.py
+++ [서버/통신]사이트 접속자수 맞추기 게임/echo_server.py
@@ -1,23 +1,18 @@
 # type: ignore
-
 import socket
-
 
 # 접속할 서버 주소입니다. 여기에서는 루프백(loopback) 인터페이스 주소 즉 localhost를 사용합니다.
 HOST = '127.0.0.1'
 # 클라이언트 접속을 대기하는 포트 번호입니다.
 PORT = 9999
 
-
 # 소켓 객체를 생성합니다.
 # 주소 체계(address family)로 IPv4, 소켓 타입으로 TCP 사용합니다.
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
 # 포트 사용중이라 연결할 수 없다는
 # WinError 10048 에러 해결를 위해 필요합니다.
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
 
 # bind 함수는 소켓을 특정 네트워크 인터페이스와 포트 번호에 연결하는데 사용됩니다.
 # HOST는 hostname, ip address, 빈 문자열 ""이 될 수 있습니다.
@@ -36,8 +31,6 @@ client_socket, addr = server_socket.accept()
 print('접속한 클라이언트 주소입니다.')
 print('Connected by', addr)
 
-
-
 # 무한루프를 돌면서
 while True:
 
@@ -54,7 +47,6 @@ while True:
 
     # 받은 문자열을 다시 클라이언트로 전송해줍니다.(에코)
     client_socket.sendall(data)
-
 
 # 소켓을 닫습니다.
 client_socket.close()
