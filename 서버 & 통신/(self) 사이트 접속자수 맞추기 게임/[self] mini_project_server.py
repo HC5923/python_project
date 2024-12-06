a5f5051cab8f3341d14b3e4f0f365d7518229988
HC5923 <ghlckd1121@gmail.com>
2024-12-06 15:01:43 +0900

    =-=

--- 서버 & 통신/(self) 사이트 접속자수 맞추기 게임/[self] mini_project_server.py
+++ 서버 & 통신/(self) 사이트 접속자수 맞추기 게임/[self] mini_project_server.py
@@ -1,58 +1,63 @@
+# multi_play_game_server.py
+
 import socket
 import select
 import random
 
-HOST = 'localhost'
-PORT = 50007
+HOST = 'localhost'   # 접속 서버 주소
+PORT = 50007         # 클라이언트 접속을 대기하는 포트번호
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print('서버 시이작~')
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    # 소켓 생성
+    s.bind((HOST, PORT))   # 소켓과 포트 연결
+    s.listen()             # 소켓 listening 시작
+    print('서버가 시작되었습니다.')
 
-    readsocks = [s]
-    answers = {}
-    num_clients = 0
+    readsocks = [s]    # select 함수에서 관찰될 소켓 리스트 생성
+    answers = {}       # 정답 딕셔너리 생성 (플레이어 별 정답 저장)
+    num_clients = 0    # 현재 접속자 수
 
     while True:
+        # select 함수로 소켓들의 상태를 확인
         readables, writeables, exceptions = select.select(readsocks, [], [])
-
+        
         for sock in readables:
-            if sock == s:
+            if sock == s:  # 신규 클라이언트 접속
                 newsock, addr = s.accept()
-                num_clients += 1
-                print(f'클라이언트가 접속했습니다.:{addr}, 정답은 {num_clients} 입니다.')
+                num_clients += 1  # 접속자 수 증가
+                print(f'클라이언트가 접속했습니다:{addr}, 정답은 {num_clients} 입니다.')
                 readsocks.append(newsock)
-                answers[newsock] = num_clients
-                for key in answers.keys():
+                answers[newsock] = num_clients  # 신규 클라이언트 정답 생성
+                for key in answers.keys():      # 기존 클라이언트 정답 업데이트
                     answers[key] = num_clients
-
-            else:
+                    
+            else:  # 이미 접속한 클라이언트의 요청 (게임진행을 위한 요청)
                 conn = sock
                 data = conn.recv(1024).decode('utf-8')
                 print(f'데이터:{data}')
 
-                try:
+                try:  # 숫자를 입력하지 않을 경우 응답 메세지
                     n = int(data)
                 except ValueError:
-                    conn.sendall(f'입력값이 올바르지 않습니다.:{data}'.encode('utf-8'))
+                    conn.sendall(f'입력값이 올바르지 않습니다:{data}'.encode('utf-8'))
                     continue
-
-                answers = answers.get(conn)
-
-                if n == 0:
+                
+                # 정답 가져오기
+                answer = answers.get(conn)
+                
+                if n == 0:  # 클라이언트가 종료 0 를 입력할 경우
                     conn.sendall(f"종료".encode('utf-8'))
                     conn.close()
-                    num_clients -= 1
-                    print(f'클라이언트가 게임을 종료했습니다.:{addr}, 정답은 {num_clients} 입니다.')
-
-                    for key in answers.keys():
+                    num_clients -= 1  # 접속자 수 감소
+                    print(f'클라이언트가 게임을 종료했습니다:{addr}, 정답은 {num_clients} 입니다.')
+                    
+                    for key in answers.keys():  # 기존 클라이언트 정답 업데이트
                         answers[key] = num_clients
-                    readsocks.remove(conn)
-
-                elif n > answers:
-                    conn.sendall("너무 높습니다.".encode('utf-8'))
-                elif n < answers:
-                    conn.sendall("너무 낮습니다.".encode('utf-8'))
+                    readsocks.remove(conn)  # 클라이언트 접속 해제시 readsocks에서 제거
+                
+                # 클라이언트의 입력값 채점
+                elif n > answer:
+                    conn.sendall("너무 높아요".encode('utf-8'))
+                elif n < answer:
+                    conn.sendall("너무 낮아요".encode('utf-8'))
                 else:
                     conn.sendall("정답입니다 !".encode('utf-8'))
