d617dc16026070fdeb8f2d2021c1a40dd1ac89e2
HC5923 <ghlckd1121@gmail.com>
2024-12-27 18:18:26 +0900

    ㅇㅅㅇ

--- Pixabay에서 이미지수집/01_search_pixabay.py
+++ Pixabay에서 이미지수집/01_search_pixabay.py
@@ -1,1 +1,10 @@
-print ("abc")
+from selenium import webdriver #selenium 모듈 설치해야함 (pip install selenium -> pip show selenium)
+
+import chromedriver_autoinstaller #chromedriver-autoinstaller 모듈 설치해야함 (pip install chromedriver-autoinstaller)
+
+chromedriver_autoinstaller.install()
+driver = webdriver.Chrome()
+driver.implicitly_wait(5)
+
+url = "https://pixabay.com/ko/"
+driver.get(url=url)
