11c3e7ab1888a3ea2557206dbb1780b52ed56dea
HC5923 <ghlckd1121@gmail.com>
2025-01-02 17:59:14 +0900

    dtd

--- Pixabay에서 이미지수집/02_multiple_pixabay.py
+++ Pixabay에서 이미지수집/02_multiple_pixabay.py
@@ -13,13 +13,18 @@ driver.implicitly_wait(5)
 url = "https://pixabay.com/ko/images/search/사과/"
 driver.get(url=url)
 
-image_xpath = "/html/body/div[1]/div[1]/div/div[2]/div[3]/div/div/div[1]/div[3]/div/a/img"
-image_url = driver.find_element(By.XPATH, image_xpath).get_attribute('src')
-print("image_url:", image_url)
+# find(), find_all() .find_elements
+image_area_xpath = "/html/body/div[1]/div[1]/div/div[2]/div[3]/div/div"
+image_area = driver.find_element(By.XPATH, image_area_xpath)
+image_elements = image_area.find_elements(By.TAG_NAME, "img")
 
-image_byte = Request(image_url, headers={'User-Agent': 'Mozilla/5.0'})
-f = open("applehahaha.jpg", "wb")
-f.write(urlopen(image_byte).read())
-f.close()
+for image_element in image_elements:
+    image_url = image_element.get_attribute("src")
+    print(image_url)
 
-sleep(300)
+# image_byte = Request(image_url, headers={'User-Agent': 'Mozilla/5.0'})
+# f = open("applehahaha.jpg", "wb")
+# f.write(urlopen(image_byte).read())
+# f.close()
+
+# sleep(300)
