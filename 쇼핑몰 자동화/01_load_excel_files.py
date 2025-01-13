30fba2c43f7b172d941aec23ee817bd37ca173a2
HC5923 <ghlckd1121@gmail.com>
2025-01-13 15:52:33 +0900

    ㅇㅅㅇ

--- 쇼핑몰 자동화/01_load_excel_files.py
+++ 쇼핑몰 자동화/01_load_excel_files.py
@@ -7,3 +7,9 @@ class ClassificationExcel:
     def __init__(self, order_xlsx_filename, partner_info_xlsx_filename):
         
         df = pd.read_excel(order_xlsx_filename)
+        df = df.rename(columns=df.iloc[1]) #1번을 열제목으로 쓰겠다
+        print(df)
+
+
+if __name__ == '__main__':
+    ce = ClassificationExcel('주문목록20221112_NEW.xlsx', '파트너목록_NEW.xlsx')
