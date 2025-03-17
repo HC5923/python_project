c3ef1e17e76b60973240d5df3ad2c29a625d1d28
HC5923 <ghlckd1121@gmail.com>
2025-03-17 10:45:13 +0900

    ㅇㅇ

--- FastAPI/test1.py
+++ FastAPI/test1.py
@@ -1,1 +1,13 @@
-print("hi")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"index": "FastAPI 안녕"}
+
+
+@app.get("/math/sum")
+def math_sum(number_1: int, number_2: int):
+    return {"result": number_1 + number_2}
+    
